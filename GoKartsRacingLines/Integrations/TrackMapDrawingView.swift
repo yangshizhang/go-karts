3dd8d832b7a0de9b6ae4a4e109ec341c185a02ef
yangshizhang <106578164+yangshizhang@users.noreply.github.com>
2026-06-05 01:20:01 +0800

    Move recording into drawing workflow

--- GoKartsRacingLines/Integrations/TrackMapDrawingView.swift
+++ GoKartsRacingLines/Integrations/TrackMapDrawingView.swift
@@ -20,20 +20,41 @@ struct CoordinateValue {
     }
 }
 
-struct TrackMapDrawingView: View {
-    let selectedTool: DrawingTool
-    let onDraw: (CoordinateValue, CGPoint, Date) -> Void
+final class DrawingMapContainer<MapView: UIView>: UIView {
+    let mapView: MapView
+    private let overlayView = UIView()
+    var coordinateForPoint: ((CGPoint) -> CLLocationCoordinate2D?)?
+    var onDraw: ((CoordinateValue, CGPoint, Date) -> Void)?
 
-    var body: some View {
-        #if canImport(MAMapKit)
-        if Bundle.main.hasAMapKey {
-            AMapDrawingRepresentable(selectedTool: selectedTool, onDraw: onDraw)
-        } else {
-            MapKitDrawingRepresentable(selectedTool: selectedTool, onDraw: onDraw)
-        }
-        #else
-        MapKitDrawingRepresentable(selectedTool: selectedTool, onDraw: onDraw)
-        #endif
+    init(mapView: MapView) {
+        self.mapView = mapView
+        super.init(frame: .zero)
+        addSubview(mapView)
+        addSubview(overlayView)
+        overlayView.backgroundColor = .clear
+        installDrawingGesture()
+    }
+
+    required init?(coder: NSCoder) { nil }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        mapView.frame = bounds
+        overlayView.frame = bounds
+    }
+
+    private func installDrawingGesture() {
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(handleDrawingPan(_:)))
+        pan.minimumNumberOfTouches = 1
+        pan.maximumNumberOfTouches = 1
+        overlayView.addGestureRecognizer(pan)
+    }
+
+    @objc private func handleDrawingPan(_ gesture: UIPanGestureRecognizer) {
+        guard gesture.numberOfTouches <= 1 else { return }
+        let point = gesture.location(in: mapView)
+        guard let coordinate = coordinateForPoint?(point) else { return }
+        onDraw?(CoordinateValue(coordinate), point, Date())
     }
 }
 
@@ -53,107 +74,68 @@ private func requireTwoFingerMapPan(on view: UIView) {
     view.subviews.forEach { requireTwoFingerMapPan(on: $0) }
 }
 
-#if canImport(MAMapKit)
-struct AMapDrawingRepresentable: UIViewRepresentable {
+struct TrackMapDrawingView: View {
     let selectedTool: DrawingTool
     let onDraw: (CoordinateValue, CGPoint, Date) -> Void
 
-    func makeCoordinator() -> Coordinator { Coordinator(onDraw: onDraw) }
+    var body: some View {
+        #if canImport(MAMapKit)
+        if Bundle.main.hasAMapKey {
+            AMapDrawingRepresentable(selectedTool: selectedTool, onDraw: onDraw)
+        } else {
+            MapKitDrawingRepresentable(selectedTool: selectedTool, onDraw: onDraw)
+        }
+        #else
+        MapKitDrawingRepresentable(selectedTool: selectedTool, onDraw: onDraw)
+        #endif
+    }
+}
 
-    func makeUIView(context: Context) -> MAMapView {
+#if canImport(MAMapKit)
+struct AMapDrawingRepresentable: UIViewRepresentable {
+    typealias UIViewType = DrawingMapContainer<MAMapView>
+    let selectedTool: DrawingTool
+    let onDraw: (CoordinateValue, CGPoint, Date) -> Void
+
+    func makeUIView(context: Context) -> UIViewType {
         let mapView = MAMapView(frame: .zero)
         MAMapView.updatePrivacyShow(.didShow, privacyInfo: .didContain)
         MAMapView.updatePrivacyAgree(.didAgree)
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .follow
         mapView.zoomLevel = 18
-        context.coordinator.attach(to: mapView)
-        return mapView
+        let container = DrawingMapContainer(mapView: mapView)
+        container.coordinateForPoint = { [weak mapView] point in mapView?.convert(point, toCoordinateFrom: mapView) }
+        container.onDraw = onDraw
+        DispatchQueue.main.async { requireTwoFingerMapPan(on: mapView) }
+        return container
     }
 
-    func updateUIView(_ uiView: MAMapView, context: Context) {
-        context.coordinator.onDraw = onDraw
-        DispatchQueue.main.async { requireTwoFingerMapPan(on: uiView) }
-    }
-
-    final class Coordinator: NSObject, UIGestureRecognizerDelegate {
-        var onDraw: (CoordinateValue, CGPoint, Date) -> Void
-        weak var mapView: MAMapView?
-
-        init(onDraw: @escaping (CoordinateValue, CGPoint, Date) -> Void) {
-            self.onDraw = onDraw
-        }
-
-        func attach(to mapView: MAMapView) {
-            self.mapView = mapView
-            requireTwoFingerMapPan(on: mapView)
-            let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
-            pan.minimumNumberOfTouches = 1
-            pan.maximumNumberOfTouches = 1
-            pan.delegate = self
-            mapView.addGestureRecognizer(pan)
-        }
-
-        func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-            false
-        }
-
-        @objc private func handlePan(_ gesture: UIPanGestureRecognizer) {
-            guard let mapView, gesture.numberOfTouches <= 1 else { return }
-            let point = gesture.location(in: mapView)
-            let coordinate = mapView.convert(point, toCoordinateFrom: mapView)
-            onDraw(CoordinateValue(coordinate), point, Date())
-        }
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        uiView.onDraw = onDraw
+        DispatchQueue.main.async { requireTwoFingerMapPan(on: uiView.mapView) }
     }
 }
 #endif
 
 struct MapKitDrawingRepresentable: UIViewRepresentable {
+    typealias UIViewType = DrawingMapContainer<MKMapView>
     let selectedTool: DrawingTool
     let onDraw: (CoordinateValue, CGPoint, Date) -> Void
 
-    func makeCoordinator() -> Coordinator { Coordinator(onDraw: onDraw) }
-
-    func makeUIView(context: Context) -> MKMapView {
+    func makeUIView(context: Context) -> UIViewType {
         let mapView = MKMapView(frame: .zero)
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .follow
-        context.coordinator.attach(to: mapView)
-        return mapView
+        let container = DrawingMapContainer(mapView: mapView)
+        container.coordinateForPoint = { [weak mapView] point in mapView?.convert(point, toCoordinateFrom: mapView) }
+        container.onDraw = onDraw
+        DispatchQueue.main.async { requireTwoFingerMapPan(on: mapView) }
+        return container
     }
 
-    func updateUIView(_ uiView: MKMapView, context: Context) {
-        context.coordinator.onDraw = onDraw
-        DispatchQueue.main.async { requireTwoFingerMapPan(on: uiView) }
-    }
-
-    final class Coordinator: NSObject, UIGestureRecognizerDelegate {
-        var onDraw: (CoordinateValue, CGPoint, Date) -> Void
-        weak var mapView: MKMapView?
-
-        init(onDraw: @escaping (CoordinateValue, CGPoint, Date) -> Void) {
-            self.onDraw = onDraw
-        }
-
-        func attach(to mapView: MKMapView) {
-            self.mapView = mapView
-            requireTwoFingerMapPan(on: mapView)
-            let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
-            pan.minimumNumberOfTouches = 1
-            pan.maximumNumberOfTouches = 1
-            pan.delegate = self
-            mapView.addGestureRecognizer(pan)
-        }
-
-        func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-            false
-        }
-
-        @objc private func handlePan(_ gesture: UIPanGestureRecognizer) {
-            guard let mapView, gesture.numberOfTouches <= 1 else { return }
-            let point = gesture.location(in: mapView)
-            let coordinate = mapView.convert(point, toCoordinateFrom: mapView)
-            onDraw(CoordinateValue(coordinate), point, Date())
-        }
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        uiView.onDraw = onDraw
+        DispatchQueue.main.async { requireTwoFingerMapPan(on: uiView.mapView) }
     }
 }
